ed1a0ed0fd60dab221a1c51ee0d1d6116c7e3b06
Thiago Resende <thiago.resende@dtidigital.com.br>
2019-12-31 12:12:52 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿
+using Newtonsoft.Json;
+using System;
+using System.Data.SqlClient;
+
+namespace SQLListener
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Listen();
+            Console.ReadKey();
+        }
+
+        private static void Listen()
+        {
+            var cs = "Data Source=.\\SQLEXPRESS;Initial Catalog=Ports;Integrated Security=True";
+            using (SqlConnection connection = new SqlConnection(cs))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("select id, name from item2 order by 1 desc", connection);
+                SqlDependency dependency = new SqlDependency(command);
+                dependency.OnChange += OnChange;
+                SqlDependency.Start(cs);
+                var item = GetItem(command);
+                command.Dispose();
+                Print(item);
+            }
+        }
+
+        private static void Print(Item item)
+        {
+            var text = JsonConvert.SerializeObject(item);
+            Console.WriteLine(text);
+        }
+
+        private static Item GetItem(SqlCommand command)
+        {
+            var item = new Item();
+            var reader = command.ExecuteReader();
+            if (reader.Read())
+            {
+                item.Id = reader.GetInt32(reader.GetOrdinal("id"));
+                item.Name = reader.GetString(reader.GetOrdinal("name"));
+            }
+            return item;
+        }
+
+        private static void OnChange(object sender, SqlNotificationEventArgs e)
+        {
+            Listen();
+        }
+    }
+}
